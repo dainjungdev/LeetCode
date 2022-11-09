8858b502c0ca99512d2ad781c7dc8091991a2aaf
Dain Jung <111036168+dainjungdev@users.noreply.github.com>
2022-11-09 18:18:25 +0000

    Time: 31 ms (46.18%), Space: 5.8 MB (90.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x > 0 && x % 10 == 0)) {
+        if (x > 0 && x % 10 == 0) {
             return false;
         }
         
         int rev = 0;
-        
         while (x > rev) {
             rev = rev * 10 + x % 10;
             x /= 10;
